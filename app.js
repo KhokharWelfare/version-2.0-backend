32338ea6614575105769b35d56a00624828c7a39
KhokharWelfare <khokharwelfare2024@gmail.com>
2025-07-06 12:06:09 +0500

    Update app.js

--- app.js
+++ app.js
@@ -11,16 +11,22 @@ const app = express();
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
-app.use(cors({ credentials: true, origin: "http://localhost:5173" }));
 
+app.use(cors({
+  credentials: true,
+  origin: [
+    "http://localhost:5173",
+    "https://www.khokharwelfarefoundaion.com"
+  ]
+}));
 // Connect to DB
 connectDB();
 
 // Routes
-app.post("/test", (req, res) => {
-  console.log("BODY:", req.body);
-  res.send(req.body);
-});
+// app.post("/test", (req, res) => {
+//   console.log("BODY:", req.body);
+//   res.send(req.body);
+// });
 app.use("/api/auth", require("./routes/authRoutes"));
 app.use("/api/donations", require("./routes/donationRoutes"));
 app.use("/api/usage", require("./routes/usageRoutes"));
