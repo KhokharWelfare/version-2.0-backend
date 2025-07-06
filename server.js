6b6cb544c82548d9a2d43a1f422d0cf90cf3505e
KhokharWelfare <khokharwelfare2024@gmail.com>
2025-07-06 20:00:47 +0500

    Update server.js

--- server.js
+++ server.js
@@ -2,8 +2,8 @@ const app = require("./app");
 
 const PORT = process.env.PORT || 5000;
 
-// app.listen(PORT, ()=>{
-//     console.log("Server running: www.localhost:5000");
-// })
+app.listen(PORT, ()=>{
+    console.log("Server running: www.localhost:5000");
+})
 
-module.exports = app;
+// module.exports = app;
